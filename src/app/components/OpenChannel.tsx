95c4007d360ee98646a69ad499c903e023a611f8
unao <artur@unao.pl>
2018-10-03 21:22:13 +0200

    opening channel re-styling and raporting progress

--- src/app/components/OpenChannel.tsx
+++ src/app/components/OpenChannel.tsx
@@ -2,15 +2,15 @@ import * as React from 'react'
 import { View, ActivityIndicator } from 'react-native'
 import {
   Header, Text, Content, Container, Button, Input, Left,
-  Icon, Body, Title, Right, Subtitle, Radio, ListItem, Item, Label, H2
+  Icon, Body, Title, Right, Subtitle, Radio, ListItem, Item, Label, H2, Card, CardItem, Spinner
 } from 'native-base'
 
 import { Address, Wei } from 'eth-types'
 import { as } from 'go-network-framework'
-import { Subject, Subscription, Observable } from 'rxjs'
+import { Subject, Subscription, Observable, BehaviorSubject } from 'rxjs'
 
 import { Account, AccountBalanceFormatted } from '../logic/accounts'
-import { openChannelAndDeposit } from '../logic/onchain-actions'
+import { openChannelAndDeposit, OpenAndDepositState, openAndDepositStateStart, OpenAndDepositSteps } from '../logic/onchain-actions'
 import { Balance } from './Balance'
 
 type Status = 'active' | 'in-progress' | 'error' | 'success'
@@ -26,9 +26,19 @@ export interface State {
   status: Status
   selectedIdx: number
   amount?: Wei
+  progress?: OpenAndDepositState
+
 }
 
 const min = (a: Wei, b: Wei) => a.gt(b) ? b : a
+
+const progressMessage: { [K in OpenAndDepositSteps]: string } = {
+  ApproveGot: 'Approve Go Network Channel Manager',
+  NewChannel: 'Create New Channel',
+  ApproveChannel: 'Approve Created Channel',
+  Deposit: 'Deposit ERC20 Tokens To Channel'
+}
+const progressOrder: OpenAndDepositSteps[] = ['ApproveGot', 'NewChannel', 'ApproveChannel', 'Deposit']
 
 export class OpenChannel extends React.Component<Props, State> {
   state: State
@@ -45,22 +55,20 @@ export class OpenChannel extends React.Component<Props, State> {
   }
 
   componentDidMount () {
-    this.sub = this.openSub
-      .do(() => this.setState({ status: 'in-progress' }))
-      .exhaustMap(s =>
-        Observable.defer(
-          () => openChannelAndDeposit(this.props.account, s.amount!, s.other)
-        )
-          .mapTo('success' as Status)
-          .catch(() => Observable.of('error' as Status))
-          .mergeMap(s =>
-            Observable.timer(3000)
-              .take(1)
-              .mapTo('active' as Status)
-              .startWith(s)
-              .do(s => this.setState({ status: s }))
+    const progress = new BehaviorSubject<OpenAndDepositState>(openAndDepositStateStart())
+    this.sub = Observable.merge(
+      progress.do(p => this.setState({ progress: p })),
+      this.openSub
+        .do(() => this.setState({ status: 'in-progress' }))
+        .exhaustMap(s =>
+          Observable.defer(
+            () => openChannelAndDeposit(this.props.account, s.amount!, s.other,
+              (k, s) => progress.next(Object.assign({}, progress.value, { [k]: { status: s } })))
           )
-      )
+            .mapTo('success' as Status)
+            .catch(() => Observable.of('error' as Status))
+        )
+    )
       .subscribe()
   }
 
@@ -78,52 +86,75 @@ export class OpenChannel extends React.Component<Props, State> {
       amount: v === '' ? undefined : min(as.Wei(v), this.props.balance.hsToken)
     })
 
+  reportProgress = () => {
+    const p = this.state.progress
+    const s = this.state.status
+    if (!p) return <ActivityIndicator /> // this should not happen
+    return <Card style={{ width: '80%', marginTop: 48, alignSelf: 'center' }}>
+      {
+        progressOrder
+          .map((k) => ({ k, p: p[k], m: progressMessage[k] }))
+          .map(({ p, m, k }) => <CardItem key={k}>
+            <View style={{ flexDirection: 'row', width: '100%', paddingRight: 72, justifyContent: 'space-between', alignItems: 'center' }}>
+              <Text note={p.status === 'not-started'}>{m}</Text>
+              {p.status === 'waiting' ?
+                <ActivityIndicator size='small' style={{ marginRight: 24 }} /> :
+                <Icon name='ios-checkmark' active={p.status !== 'not-started'}
+                  style={{ color: p.status === 'ok' ? 'green' : 'rgba(128,128,128,0.6)' }} />
+              }
+            </View>
+          </CardItem>)
+      }
+    </Card>
+  }
+
   renderContent () {
     const without = this.props.accountsWithoutChannel
 
-    if (without.length === 0) {
-      return <Text>No accounts without openned channel found.</Text>
-    }
-
-    if (!this.enoughTokens()) {
-      return <Text>You need to have some tokens to be able to open new netting channel. Please use a master account instead.</Text>
-    }
-
     switch (this.state.status) {
-      case 'active': return <View>
-        <View style={{ flexDirection: 'row', padding: 8, justifyContent: 'space-around', alignItems: 'center' }}>
-          <Item floatingLabel style={{ maxWidth: '30%' }}>
-            <Label>Amount</Label>
-            <Input
-              placeholder={`max: ${this.props.balance.gotToken.toString(10)}`}
-              value={this.state.amount ? this.state.amount.toString(10) : ''}
-              onChangeText={this.updateAmount}
-              keyboardType='number-pad'
-            />
-          </Item>
-          <Button disabled={!this.canOpen()} style={{ margin: 8, alignSelf: 'center' }}
-            onPress={this.onOpen}
-          >
-            <Text>Open</Text>
-          </Button>
-        </View>
-        <H2>Select Peer</H2>
-        {without.map((w, idx) =>
-          <ListItem key={w.addressStr} onPress={() => this.state.selectedIdx = idx}>
-            <Left><Text>0x{w.addressStr}</Text></Left>
-            <Right>
-              <Radio selected={this.state.selectedIdx === idx} />
-            </Right>
-          </ListItem>
-        )}
-        <Text note style={{ marginTop: 16 }}>Only one netting channel may be active between two Ethereum addresses.
-          To open another one the previous one needs to settle.
-          The above selection excludes address which have open channel.
+      case 'active': {
+        if (without.length === 0) {
+          return <Text>No accounts without openned channel found.</Text>
+        }
+
+        if (!this.enoughTokens()) {
+          return <Text>You need to have some tokens to be able to open new netting channel. Please use a master account instead.</Text>
+        }
+        return <View>
+          <View style={{ flexDirection: 'row', padding: 8, justifyContent: 'space-around', alignItems: 'center' }}>
+            <Item floatingLabel style={{ maxWidth: '30%' }}>
+              <Label>Amount</Label>
+              <Input
+                placeholder={`max: ${this.props.balance.gotToken.toString(10)}`}
+                value={this.state.amount ? this.state.amount.toString(10) : ''}
+                onChangeText={this.updateAmount}
+                keyboardType='number-pad'
+              />
+            </Item>
+            <Button disabled={!this.canOpen()} style={{ margin: 8, alignSelf: 'center' }}
+              onPress={this.onOpen}
+            >
+              <Text>Open</Text>
+            </Button>
+          </View>
+          <H2>Select Peer</H2>
+          {without.map((w, idx) =>
+            <ListItem key={w.addressStr} onPress={() => this.state.selectedIdx = idx}>
+              <Left><Text>0x{w.addressStr}</Text></Left>
+              <Right>
+                <Radio selected={this.state.selectedIdx === idx} />
+              </Right>
+            </ListItem>
+          )}
+          <Text note style={{ marginTop: 16 }}>Only one netting channel may be active between two Ethereum addresses.
+            To open another one the previous one needs to settle.
+            The above selection excludes address which have open channel.
         </Text>
-      </View>
-      case 'in-progress': return <ActivityIndicator />
-      case 'success': return <Text style={{ color: 'green' }}>Channel Opened Successfully!</Text>
-      case 'error': return <Text style={{ color: 'red' }}>Opening Channel Failed</Text>
+        </View>
+      }
+      case 'success':
+      case 'error':
+      case 'in-progress': return this.reportProgress()
     }
   }
 
--- src/app/logic/onchain-actions.ts
+++ src/app/logic/onchain-actions.ts
@@ -6,32 +6,61 @@ const log = <T> (msg: string, logValue = false, ...rest: any[]) => (p: T): Promi
   return Promise.resolve(p)
 }
 
-export const openChannel = (account: Account, amount: Wei, other: Address) =>
-  account.txs.approve({ to: account.contracts.gotToken },
+export type ActionStepStatus = 'not-started' | 'waiting' | 'ok' | 'error'
+
+export type OpenAndDepositSteps = 'ApproveGot' | 'NewChannel' | 'ApproveChannel' | 'Deposit'
+
+export type OpenAndDepositState = { [K in OpenAndDepositSteps]: { status: ActionStepStatus, message?: string } }
+
+export const openAndDepositStateStart: () => OpenAndDepositState = () => ({
+  ApproveGot: { status: 'not-started' },
+  NewChannel: { status: 'not-started' },
+  ApproveChannel: { status: 'not-started' },
+  Deposit: { status: 'not-started' }
+})
+
+type LogFn = (k: OpenAndDepositSteps, s: ActionStepStatus, message?: string) => void
+
+export const openChannel = (account: Account, amount: Wei, other: Address, log: LogFn) => {
+  log('ApproveGot', 'waiting')
+  return account.txs.approve({ to: account.contracts.gotToken },
     { _spender: account.contracts.manager, _value: amount })
-    .then(log('APPROVED'))
     .then(() => {
+      log('ApproveGot', 'ok')
+      log('NewChannel', 'waiting')
       return account.txs.newChannel({ to: account.contracts.manager },
         { partner: other, settle_timeout: account.engine.settleTimeout })
     })
-    .then(logs =>
-      (logs.filter(x => x._type === 'ChannelNew')[0] as any).netting_channel)
-    .then(log('CREATED'))
+    .then(logs => {
+      log('NewChannel', 'ok')
+      return (logs.filter(x => x._type === 'ChannelNew')[0] as any).netting_channel
+    })
+}
 
-export const deposit = (account: Account, token: Address, channel: Address, amount: Wei) =>
-  account.txs.approve({ to: token }, { _spender: channel, _value: amount })
-    .then(() => account.txs.deposit({ to: channel }, { amount: amount }))
+export const deposit = (account: Account, token: Address, channel: Address, amount: Wei, log: LogFn) => {
+  log('ApproveChannel', 'waiting')
+  return account.txs.approve({ to: token }, { _spender: channel, _value: amount })
+    .then(() => {
+      log('ApproveChannel', 'ok')
+      log('Deposit', 'waiting')
+      account.txs.deposit({ to: channel }, { amount: amount })
+        .then(d => {
+          log('Deposit', 'ok')
+          return d
+        })
+    })
+}
 
-export const openChannelAndDeposit = (account: Account, amount: Wei, other: Address) =>
+export const openChannelAndDeposit = (account: Account, amount: Wei, other: Address, log: LogFn) =>
   Promise.all([
     account.blockchain.monitoring.asStream('ChannelNewBalance')
+      .skip(1)
       .take(1)
       .delay(0)
       .toPromise(),
-    openChannel(account, amount, other)
-      .then(ch => deposit(account, account.contracts.testToken, ch, amount)
+    openChannel(account, amount, other, log)
+      .then(ch => deposit(account, account.contracts.testToken, ch, amount, log)
         .then(() => ({ channel: ch }))
-        .then(log(`CREATED AND DEPOSITED ${amount.toString()}$ chan: 0x${ch.toString('hex')} from: 0x${account.owner.addressStr} to: 0x${other.toString('hex')}`))
       )
   ])
     .then(([_, x]) => x)
